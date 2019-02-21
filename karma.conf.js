1a1af14a0a48d1889e8b8c6afe6d8d164957a9b0
Christian Catalan <crcatala@gmail.com>
2019-02-21 12:42:49 -0600

    hostname linux

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,7 @@
 module.exports = function(config) {
   config.set({
     basePath: "./",
+    hostname: process.platform === "linux" ? "127.0.0.1" : "localhost",
     frameworks: ["mocha", "chai"],
     reporters: ["mocha"],
     mochaReporter: {
